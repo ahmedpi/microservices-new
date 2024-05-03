6efa5aba2a313257dea25424dd50868db3990abf
ahmedpi <ahmedali.pisa@gmail.com>
2024-05-03 10:54:27 +0200

    service discovery

--- order-service/src/main/java/com/ahmedproject/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/ahmedproject/orderservice/OrderServiceApplication.java
@@ -3,11 +3,11 @@ package com.ahmedproject.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-	@SpringBootApplication
+@SpringBootApplication
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 
 }
